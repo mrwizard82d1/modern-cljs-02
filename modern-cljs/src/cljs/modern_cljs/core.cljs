5a1e146e1507a8d75a24e6d190f4c8d2a019dd8c
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-12-20 08:54:01 -0600

    Implement code from tutorial 1.
    Implement code to create first ("Hello World") web page.

---
+++ modern-cljs/src/cljs/modern_cljs/core.cljs
@@ -0,0 +1,8 @@
+;; create the main project namespace
+(ns modern-cljs.core)
+
+;; enable cljs to print to the JavaScript console of the browser
+(enable-console-print!)
+
+;; print a friendly greeting to the console
+(println "Hello, Modern ClojureScript World!")
